fd4fe2c8a8fb16c661356921b8da655ec62a9125
Griotold <gotjd9773@naver.com>
2023-10-03 13:29:49 +0900

    Section 1. 예제 만들기 - 로그 추적기 V2 파라미터로 동기화 개발

---
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV2Test.java
@@ -0,0 +1,30 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class HelloTraceV2Test {
+
+    @Test
+    @DisplayName("정상 종료")
+    void begin_end_level2() throws Exception {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    @DisplayName("예외 발생")
+    void begin_exception_level2() throws Exception {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+
+}
