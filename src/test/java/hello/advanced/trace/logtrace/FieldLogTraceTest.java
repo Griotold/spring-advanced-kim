6887c05dfd73fc43e83dd06bab77f6e7f932d320
Griotold <gotjd9773@naver.com>
2023-10-05 12:34:00 +0900

    Section 2. 쓰레드 로컬, Thread Local - 필드 동기화 - 개발

---
+++ src/test/java/hello/advanced/trace/logtrace/FieldLogTraceTest.java
@@ -0,0 +1,30 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldLogTraceTest {
+
+    FieldLogTrace trace = new FieldLogTrace();
+
+    @Test
+    @DisplayName("필드 동기화")
+    void begin_end_level2() throws Exception {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    @DisplayName("필드 동기화 - 예외")
+    void begin_exception_level2() throws Exception {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+}
